a2890652b4802bdbf870d363804822e242cecebc
shahstudios <sshahcode@gmail.com>
2021-06-10 00:35:56 -0400

    add js function to check input if it is a valid hex

--- color-tool/index.js
+++ color-tool/index.js
@@ -1,1 +1,8 @@
 // javascript
+
+const isValidHex = (hex) => {
+    if(!hex) return false;
+    
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
