f2f3615f9e7435df39d9ce1bedd0dea09d1a87b1
shahstudios <sshahcode@gmail.com>
2021-06-12 20:04:33 -0400

    create alter color function for slider input

--- color-tool/index.js
+++ color-tool/index.js
@@ -3,6 +3,8 @@
 // Get a reference to DOM elements
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
 
 // keyup event handler user input on hex code
 hexInput.addEventListener('keyup', () => {
@@ -14,6 +16,7 @@ hexInput.addEventListener('keyup', () => {
     inputColor.style.backgroundColor = '#' + strippedHex;
 })
 
+// Create function to do catch no valid hex inputs
 const isValidHex = (hex) => {
     // Check if hex color is valid
     if (!hex) return false;
@@ -22,6 +25,7 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
+// Create function to convert the hex input into rgb
 convertHexToRgb = (hex) => {
     if (!isValidHex(hex)) return null;
     let strippedHex = hex.replace('#', '');
@@ -44,6 +48,7 @@ console.log(convertHexToRgb("000000"));
 console.log(convertHexToRgb("ffe"));
 */
 
+// Create a function to convert rgb input to hex return
 const convertRgbToHex = (r, g, b) => {
     const firstParameter = ("0" + r.toString(16)).slice(-2);
     const secondParameter = ("0" + g.toString(16)).slice(-2);
@@ -54,4 +59,32 @@ const convertRgbToHex = (r, g, b) => {
 }
 // console.log(convertRgbToHex(0, 55, 55));
 
+// Create a event listener for the slider 
+slider.addEventListener('input', () => {
+    sliderText.textContent = `${slider.value}%`;
+})
 
+// Increase range between 0 - 255
+const increaseRange = (hex, amount) => {
+    const newHex = hex + amount;
+    if(newHex > 255) return 255;
+    if (newHex < 0) return 0;
+    return newHex;
+}
+
+const alterColor = (hex, percentage) => {
+    const rgbValues = convertHexToRgb(hex);
+    const amount = Math.floor((percentage/100) * 255);
+
+    const newR = increaseRange(rgbValues.r, amount);
+    const newG = increaseRange(rgbValues.g, amount);
+    const newB = increaseRange(rgbValues.b, amount);
+
+    console.log({newR, newG, newB});
+    return convertRgbToHex(newR, newG, newB);
+}
+
+
+
+
+alterColor("fff", 10)
